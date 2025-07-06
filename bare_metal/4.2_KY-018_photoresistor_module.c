580cea64d3cbc9dda780b654c3167a8305713896
thongnm91 <33078384+thongnm91@users.noreply.github.com>
2025-07-06 23:32:35 +0300

    1

--- bare_metal/4.2_KY-018_photoresistor_module.c
+++
@@ -1,9 +0,0 @@
-/*
-Lumen = Lux * met square (m2)
-An LED 200 lumen = 200 lux in 1 met square. 
-*/
-float calLux(uint32_t adc_raw){
-	float vol = adc_raw*(3.3/4096); //Convert current voltage
-	float lux = (3.3-vol)/0.00056; //0.00056V per 1 lux.
-	return lux;
-}
